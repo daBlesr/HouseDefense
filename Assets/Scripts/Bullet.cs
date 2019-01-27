a3eadefcae4c5719202826b3dfd27b7e4f0f1d00
NielsDrost <codingdutchman@gmail.com>
2019-01-27 13:35:25 +0100

    arrow ignore platform

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
 	[SerializeField] private GameObject secondLayer;
+    private float timeAlive;
 
-    private void Update()
+    void Start()
+    {
+        this.timeAlive = Time.time;
+    }
+
+    void Update()
     {
         gameObject.transform.Rotate(Vector3.forward, - Time.deltaTime * 30);
+
+        if (timeAlive > 30.0f)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.tag != "Player")
-        {
-            Destroy(gameObject);
-        } else
+        if (collision.collider.tag == "Player")
         {
             Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), collision.collider);
-		}
+        }
 	}
 }
